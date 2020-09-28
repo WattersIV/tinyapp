4c0a2d9713c38cba0ec12e02ac28076b9cb7f7b5
Bill Watters <billywatters13@gmail.com>
2020-09-28 19:56:10 +0000

    Added ability to search a short url to find the long url at /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -21,6 +21,13 @@ app.get("/urls", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); 
+app.get("/urls/:shortURL", (req, res) => { 
+  console.log(req.params)
+  const templateVars = { shortURL: req.params.shortURL, 
+                          longURL: urlDatabase[req.params.shortURL] }; 
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
