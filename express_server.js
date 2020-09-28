c018fe16cabc5f9977a6f2c5a2d086f9053be9f3
Bill Watters <billywatters13@gmail.com>
2020-09-28 22:37:11 +0000

    Added logic in create new url to redirect to new short URL page

--- express_server.js
+++ express_server.js
@@ -4,28 +4,28 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 
+//------------------- Data Base and Functions ---------
 const generateRandomString = () => {
   let result           = '';
   const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for ( var i = 0; i <= 5; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 }
-
-app.use(bodyParser.urlencoded({extended: true})); //transforms body data from buffer to a string
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-}); 
+//-----------URL Generation/Lookup-----------------
+
+app.use(bodyParser.urlencoded({extended: true})); //transforms body data from buffer to a string
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(); 
+  urlDatabase[shortURL] = req.body.longURL; //adds key value pair to the object
+  res.redirect(`/urls/${shortURL}`);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });  
@@ -33,9 +33,6 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase}; 
   res.render("urls_index", templateVars);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); 
 app.get("/urls/new", (req, res) => {     // gen a new short url
   res.render("urls_new");
 });
@@ -46,6 +43,14 @@ app.get("/urls/:shortURL", (req, res) => {    //find longurl with short
                           longURL: urlDatabase[req.params.shortURL] }; 
   console.log(templateVars);
   res.render("urls_show", templateVars);
+}); 
+
+// --------------Misc----------------------
+app.get("/", (req, res) => {
+  res.send("Hello!");
+}); 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 }); 
 
 app.listen(PORT, () => {
