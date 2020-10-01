9d48ac0ce752a13aeef185174cb36bf8d5866332
Bill Watters <billywatters13@gmail.com>
2020-10-01 20:41:59 +0000

    Added mocha and chai testing

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { getEmail } = require('../helpers.js');
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getEmail("user@example.com", users)
+    const expectedOutput = "userRandomID";
+    assert(expectedOutput, user)
+  });
+  it('should return false without a valid email', function() {
+    const user = getEmail("Notsavedemail@abc.ca", users)
+    const expectedOutput = undefined;
+    assert(false === user)
+  });
+});
